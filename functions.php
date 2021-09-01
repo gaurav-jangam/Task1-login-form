e937d67778defb3b0c2f274868b766fff1391446
Gaurav <gauravjangam444@gmail.com>
2021-09-01 12:07:32 +0530

    Done till User-Defined functions

--- functions.php
+++ functions.php
@@ -16,7 +16,44 @@
         say_hello2("world");
         say_hello2("Everyone");
 
+        $name = "Gaurav Jangam";
+        say_hello2($name);
 
+        function say_hello3($greeting, $target, $punct) {
+            echo $greeting . ", " . $target . $punct . "<br/>" ;
+        }
+
+        say_hello3("hello", $name, "!");
     ?>
+    <br/>
+        <br/>
+        <?php
+        function addition(){
+            echo 2 + 3 . "<br/>";
+        }
+        addition();
+
+        
+
+        function addition1($var1, $var2){
+            return $var1 + $var2;
+        }
+        $sum = addition1(3,7);
+        echo $sum;
+        if(addition1(5,6) == 11 ){
+           echo "yes";
+        }
+        function add_sub($val1, $val2){
+            $add = $val1 + $val2;
+            $sub = $val1 - $val2;
+            $result = array($add, $sub);
+            return $result;
+        }
+        $result_array = add_sub(10,5);
+        echo "Add:" . $result_array[0];
+        echo "Sub:" . $result_array[1];
+
+        ?>
+
 </body>        
 </html>
---
+++ functions2.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>
+        Functions2
+        </title>
+    <head>
+    <body>
+    <?php
+    $bar = "outside";
+    function foo(){
+        global $bar;
+        $bar = "inside";
+    }
+    foo();
+    echo $bar . "<br/>";
+    ?>
+    <br/>
+    <?php
+    $bar = "outside";
+    function foo2($var){
+        $var = "inside";
+        return $var;
+    }
+    $bar = foo2($bar);
+    echo $bar . "<br/>";
+    ?>
+    <?php
+    function paint($room = "Hall",$color = "orange" ){
+        echo "The color of the {$room} is {$color}";
+    }
+    paint("$room","blue");
+    ?>
+</body>        
+</html>
